133f2c3a2c892279d9d507f89f6bd8369ea525d0
Moises Miguel <moimigueldev@gmail.com>
2023-10-27 19:30:38 -0500

    completed full algo

--- collection/maze_generator_pt2/sketch.js
+++ collection/maze_generator_pt2/sketch.js
@@ -1,8 +1,9 @@
 let cols, rows;
 
-const w = 40;
+const w = 16;
 const grid = [];
 let current;
+const stack = [];
 
 function setup() {
   createCanvas(400, 400);
@@ -21,17 +22,30 @@ function setup() {
 
 function draw() {
   background(100);
+  // for (let i = 0; i < stack.length; i++) {
+  //   stack[i].highlightStack();
+  // }
 
   for (let i = 0; i < grid.length; i++) {
     grid[i].show();
   }
 
   current.visited = true;
+  current.highlight();
   const next = current.checkNeighbors();
 
   if (next) {
     next.visited = true;
+    stack.push(current);
+    removeWalls(current, next);
     current = next;
+  } else {
+    if (stack.length) {
+      current = stack.pop();
+    } else {
+      console.log("DONE");
+      noLoop();
+    }
   }
 
   // noLoop();
@@ -52,6 +66,17 @@ function Cell(i, j) {
 
   // top, right, bottom, left
   this.walls = [true, true, true, true];
+
+  this.highlight = function () {
+    noStroke();
+    fill(0, 255, 0, 255, 100);
+    rect(i * w, j * w, w, w);
+  };
+  this.highlightStack = function () {
+    noStroke();
+    fill(255, 0, 0, 255);
+    rect(i * w, j * w, w, w);
+  };
 
   this.checkNeighbors = function () {
     const neighbors = [];
@@ -105,8 +130,31 @@ function Cell(i, j) {
     }
 
     if (this.visited) {
+      noStroke();
       fill(255, 0, 255, 100);
       rect(x, y, w, w);
     }
   };
+}
+
+function removeWalls(a, b) {
+  const x = a.i - b.i;
+
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+
+  const y = a.j - b.j;
+
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
 }
